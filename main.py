18ffbd1a182ce966a7750ad6e0a9124cc1321184
Obada <obadaatarking@gmail.com>
2025-08-04 00:18:32 +0300

    طباعة وانواع بيانات

---
+++ main.py
@@ -0,0 +1,49 @@
+print("hello") 
+
+#print   امر الطباعة
+#  fC amar
+
+#اي شي ما بدك حدا يشوفه  
+
+print(type("obada"))#كلمة
+
+#str  =  streng
+
+print(type(10))  #رقم صحيح
+
+#int  =  intger 
+
+print(type(100.9))  #  رقم عشري 
+
+#Float  =  Float 
+
+print(type("truo and folse")) 
+
+print(type(2 == 2))#True = 1
+
+print(type(2 == 4))#faLas = 10 
+
+#bool   =   boolean 
+
+print(type((1 , 2 , 3 , 4 , 5))) #    قأمه مستحيل تغيرها ازا بلش الأمر 
+
+#tuple   =   Tuplo
+
+print(type([1 , 2 , 3 , 4 , 5]))#    قأمه ممكن تغيرها حتا لو شغلت ألأمر
+
+#list  =  List
+
+print(type({"one": 1,   "two":   2,   "three":   3  }))#  علا حسب ما فهمت هي مفتاح و قيمه
+
+#Dick   =   Dicktionary
+
+
+
+
+
+
+
+
+
+
+
