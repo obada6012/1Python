09df0052156e3ac7bcf676c605ebd1896a093f8c
Obada <obadaatarking@gmail.com>
2025-08-04 23:49:10 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,59 @@
+# help("keywords")
+#لا تستطيع تسميه مجلد فيهم 
+#تستطيع كتابت المتغير و هو لحاله بيعرف مثل
+x = 10
+y = "obada"
+o , b , a , = 1 , 2 , 3
+
+print(x)
+
+print(y)
+
+print(o)
+
+print(b)
+
+print(a)
+
+
+print("hello\bworld")# بيحذف حرف من لي قبله
+
+print("i \
+Love \
+mom")  #ازا نزلت صور تحطها من شان يكمل قرأه
+
+print("obada \\") # وحده تلغي تأثير الثانية
+
+print('amar')#' و "  نفس الشي
+
+print("obada\natar") # تنزيل سطر
+
+print("obada\tobada") #  تعمل مسافر ال tob
+
+print("101010\robada")#  علا كل حرف تحذف رقم 
+
+print("\x6f")
+
+#concanatoin هي ل دمج اي نصين 
+
+print("obada" + " " + "obada")
+print("obada" + "obada")
+print("obada" + "\n" +"obada")
+
+#print("hello + 1") error  لا يمكن استخدا نص مع رقم 
+
+
+n = "obada"
+
+m = "atar"
+
+print( n + m )
+
+
+print("obada 'obada' obada")
+
+
+print('obada "obada" obada')
+
+
+# الترابيل تحدد كل ما بينهم وتلغي تأثير اي علامه ما عدا /\ازا وضعت اخر السطؤةو بعد شي مباشر
