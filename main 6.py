c1b8330ae592b951d5bb16f8eb5e65d0e161034f
Obada <obadaatarking@gmail.com>
2025-08-12 00:30:27 +0300

    Add files via upload

---
+++ main 6.py
@@ -0,0 +1,67 @@
+print("the name of God")#zطريقة قديمة
+
+name = "obada"
+
+age = 14
+
+rank = 5
+
+
+print("my name is: %s" % "obada")
+
+print("my age is:%d" % 14)
+
+print("my rank is:%f" % 5)
+
+
+print("my name is: %s obada my age is: %d 14 and  my rank is 5 ")
+
+
+# %s -> streng 
+# %d -> number 
+# %f -> float
+
+
+o = "obada"
+
+b = "king"
+
+a = 10
+
+print("my name is %s Iam %s my rank %d" % (o, b, a))
+
+
+
+
+
+
+print("__string formatting__")#طريقة جديدة
+#1_{:s} => string
+#نفس الفكرة مع تغيرات بسيطة واشياء جديدة 
+#هذه لست مطر لتحديد نوع البيانات هو ياخذ بترتيب
+print("___{:S}___")
+a1 = 'amar'
+print("my name is : {:S}".format(a1)) 
+#تحديد عدد الحروف المراد طباعتاها
+print("my name is : {:.2s}".format(a1)) 
+
+#2_{d} => number
+print("___{:d}___")
+a2 = 17
+print("my age is : {:d}".format(a2))
+ 
+#3_{:f} => float
+print("___{:f}___")
+a3 = 10
+print("i have A : {:f}".format (a3)) 
+#تحديد عدد الحروف المراد طباعتاها
+print("i have A : {:.3f}".format(a3)) 
+
+
+print("__new__")
+
+
+
+
+
+
